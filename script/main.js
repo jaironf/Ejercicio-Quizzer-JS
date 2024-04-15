0e30bff73f468876099cecf154b3c096659899d5
jaironf <jaironf.94@gmail.com>
2024-04-15 17:11:07 +0200

    Añadido respuestas en la pantalla

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 console.log('hola caracola');
 
-const API_URL = 'https://quizapi.io/api/v1/questions?apiKey=UYtp5zUUULMmeKVlOf3MN7YzxgdtWj95UWtsmOI4&limit=10';
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple';
 const questionDiv = document.getElementById('questions-Div')
 const homeNavBtn = document.getElementById('home');
 const gameNavBtn = document.getElementById('game');
@@ -9,28 +9,40 @@ const statsNavBtn = document.getElementById('stats');
 // console.log(homeNavBtn, gameNavBtn, statsNavBtn);
 
 
+let currentQuestionIndex = 0;
+
+
 const printQuestions = (getQuestions) => {
     console.log(getQuestions);
-    questionDiv.innerHTML = "";
-    
-  getQuestions.forEach((question) => {
-    console.log(question.question)
+    questionDiv.innerHTML = ""; 
+
     questionDiv.innerHTML = `
     <div class="card-header">Question: </div>
     <div class="card-body">
-      <p class="card-text">${question.question}</p>`;
-    });
+      <p class="card-text">${getQuestions[currentQuestionIndex].question}</p>`
+
+    let correctButton = document.createElement("button")
+    correctButton.textContent = getQuestions[currentQuestionIndex].correct_answer
+    console.log(correctButton);
+    
+    questionDiv.appendChild(correctButton)
+
+    for (let i = 0; i < getQuestions[currentQuestionIndex].incorrect_answers.length; i++) {
+        let incorrectButton = document.createElement("button")
+        incorrectButton.textContent = getQuestions[currentQuestionIndex].incorrect_answers[i]
+        questionDiv.appendChild(incorrectButton)
+    }
   };
 
 
+
+
 const getQuestions = async () =>{
-    try {
-        const response = await axios.get(API_URL);
-        const questionsArr = response.data
+    
+        let response = await axios.get(API_URL);
+        let questionsArr = response.data.results
         printQuestions(questionsArr)
-    } catch (error) {
-        console.error(error);
-    }
+    
 };
 
 getQuestions();
